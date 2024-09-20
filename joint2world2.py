41ce7a01f942fc3ddbcdeea1f70d596d2b85dad4
mac <1208876684@qq.com>
2024-09-20 22:28:33 +0800

    feature: add function of dragging files

---
+++ joint2world2.py
@@ -0,0 +1,168 @@
+import pandas as pd
+import os
+import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+RAD_ANGLE = 180.0 / np.pi
+arm_length = 225 #TODO: connect to robot type
+ALL = 1
+
+class SC_MOD:
+    def __init__(self):
+        self.d1 = 10
+        self.d3 = 10
+        self.d4 = 10
+        self.d5 = 10
+        self.L3 = 225
+        self.L4 = 225
+        self.L5 = 320
+        self.Lm3 = 10
+        self.Lm4 = 10
+        self.Lm5 = 10
+sc_mod = SC_MOD()
+
+class TRANS:
+    def __init__(self):
+        self.n = np.zeros(3)
+        self.o = np.zeros(3)
+        self.a = np.zeros(3)
+        self.p = np.zeros(3)
+
+def create_transform_matrix(p1,p2,p3):
+    t = TRANS()
+    t.n[0] = p1[0]
+    t.n[1] = p1[1]
+    t.n[2] = p1[2]
+    t.o[0] = p2[0]
+    t.o[1] = p2[1]
+    t.o[2] = p2[2]
+    t.a = [0, 0, 1]
+    t.p[0] = p3[0]
+    t.p[1] = p3[1]
+    t.p[2] = p3[2]
+    return t
+
+def mm_multi(t1, t2, choice):
+    result = TRANS()
+
+    result.n[0] = t1.n[0] * t2.n[0] + t1.o[0] * t2.n[1] + t1.a[0] * t2.n[2]
+    result.n[1] = t1.n[1] * t2.n[0] + t1.o[1] * t2.n[1] + t1.a[1] * t2.n[2]
+    result.n[2] = t1.n[2] * t2.n[0] + t1.o[2] * t2.n[1] + t1.a[2] * t2.n[2]
+
+    result.o[0] = t1.n[0] * t2.o[0] + t1.o[0] * t2.o[1] + t1.a[0] * t2.o[2]
+    result.o[1] = t1.n[1] * t2.o[0] + t1.o[1] * t2.o[1] + t1.a[1] * t2.o[2]
+    result.o[2] = t1.n[2] * t2.o[0] + t1.o[2] * t2.o[1] + t1.a[2] * t2.o[2]
+
+    result.a[0] = t1.n[0] * t2.a[0] + t1.o[0] * t2.a[1] + t1.a[0] * t2.a[2]
+    result.a[1] = t1.n[1] * t2.a[0] + t1.o[1] * t2.a[1] + t1.a[1] * t2.a[2]
+    result.a[2] = t1.n[2] * t2.a[0] + t1.o[2] * t2.a[1] + t1.a[2] * t2.a[2]
+
+    if choice == 'ALL':
+        result.p[0] = t1.n[0] * t2.p[0] + t1.o[0] * t2.p[1] + t1.a[0] * t2.p[2] + t1.p[0]
+        result.p[1] = t1.n[1] * t2.p[0] + t1.o[1] * t2.p[1] + t1.a[1] * t2.p[2] + t1.p[1]
+        result.p[2] = t1.n[2] * t2.p[0] + t1.o[2] * t2.p[1] + t1.a[2] * t2.p[2] + t1.p[2]
+
+    return result
+
+def EFEM3x_joint_to_jtin(joint):
+    jtin = np.zeros(4)
+    jtin[0] = joint[0]
+    jtin[1] = joint[1]
+    jtin[2] = np.arcsin(joint[2] / (2.0 * arm_length)) * RAD_ANGLE
+    jtin[3] = joint[3]
+    return jtin
+
+def EFEM3x_joint_to_world(jtin):
+    T01 = create_transform_matrix([1,0,0,],[0,1,0],[0,0,jtin[0]+sc_mod.d1])
+    T12 = create_transform_matrix([np.cos(np.radians(jtin[1])), np.sin(np.radians(jtin[1])), 0],
+                                  [-np.sin(np.radians(jtin[1])), np.cos(np.radians(jtin[1])), 0],
+                                  [0, 0, 0])
+    T23 = create_transform_matrix([np.cos(np.radians(jtin[2])), np.sin(np.radians(jtin[2])), 0],
+                                  [-np.sin(np.radians(jtin[2])), np.cos(np.radians(jtin[2])), 0],
+                                  [0, 0,sc_mod.d3])
+    T34 = create_transform_matrix([np.cos(np.radians(180 - 2 * jtin[2])), np.sin(np.radians(180 - 2 * jtin[2])), 0],
+                                  [-np.sin(np.radians(180 - 2 * jtin[2])), np.cos(np.radians(180 - 2 * jtin[2])), 0],
+                                  [sc_mod.L3, 0, sc_mod.d4])
+    T45 = create_transform_matrix([np.cos(np.radians(-90 + jtin[2])), np.sin(np.radians(-90 + jtin[2])), 0],
+                                  [-np.sin(np.radians(-90 + jtin[2])), np.cos(np.radians(-90 + jtin[2])),0],
+                                  [sc_mod.L4, 0, sc_mod.d5])
+    T5E = create_transform_matrix([1, 0, 0],
+                                  [0, 1, 0],
+                                  [sc_mod.L5, 0, 0])
+
+    Tmp1 = mm_multi(T01, T12, 'ALL')
+    Tmp2 = mm_multi(Tmp1, T23, 'ALL')
+    Tmp1 = mm_multi(Tmp2, T34, 'ALL')
+    Tmp2 = mm_multi(Tmp1, T45, 'ALL')
+    t = mm_multi(Tmp2, T5E, 'ALL')
+
+    return t
+
+def joint_to_world(joint):
+    jtin = EFEM3x_joint_to_jtin(joint)
+    t6 = EFEM3x_joint_to_world(jtin)
+    return t6
+
+def process_txt_file(file_path):
+    df = pd.read_csv(file_path, sep=r'\s*:\s*|\s+|,|;', engine='python', header=None)
+    return df
+
+def process_data(file_path):
+    file_name = os.path.basename(file_path)
+    number = ''.join(filter(str.isdigit, file_name))
+    output_file_path = os.path.join(file_path, f"out_data_{number}.xlsx")
+
+    txt_file_path = os.path.join(file_path,"out_data.txt")
+    data = process_txt_file(txt_file_path)
+    with pd.ExcelWriter(output_file_path, engine='openpyxl') as writer:
+        data.to_excel(writer, index=False, sheet_name='Sheet1')
+        print(f"DATA WRITTEN TO {output_file_path} - Sheet1")
+
+    z = data.iloc[:,5].to_numpy()
+    t = data.iloc[:,9].to_numpy()
+    r = data.iloc[:,13].to_numpy()
+    x = data.iloc[:,17].to_numpy()
+    joint = []
+    xx = []
+    yy = []
+    zz = []
+    for i in range(len(x)):
+        index = i-1
+        joint.append([z[index],t[index],r[index],x[index]])
+        result = joint_to_world(joint[i])
+        xx.append(result.p[0])
+        yy.append(result.p[1])
+        zz.append(result.p[2])
+
+    vx = np.zeros(len(xx))
+    vy = np.zeros(len(yy))
+    vx[1:] = np.diff(xx)
+    vy[1:] = np.diff(yy)
+
+    data_second = {
+        'x':xx,
+        'y':yy,
+        'z':zz,
+        'vx': vx,
+        'vy': vy
+    }
+
+    df_second = pd.DataFrame(data_second)
+
+    try:
+        with pd.ExcelWriter(output_file_path, engine='openpyxl', mode='a') as writer:
+            df_second.to_excel(writer, index=False, sheet_name='Sheet2')
+            print(f"DATA WRITTEN TO {output_file_path} - Sheet2")
+    except Exception as e:
+        print(f"Error writing data: {e}")
+
+    plt.figure(figsize=(8, 8))  #FIXME: 多次出现跳点，需check
+    plt.plot(xx, yy, marker='o', linestyle='-', color='b')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Track')
+    plt.grid(True)
+    plt.axis('equal')  # 保持x和y的比例相�?
+    plt.show()
---
+++ main2.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+import tkinter as tk
+import os
+from tkinterdnd2 import DND_FILES, TkinterDnD
+from joint2world2 import process_data
+from joint2world_5axes import process_data2
+
+
+class App:
+    def __init__(self, root):
+        self.root = root
+        self.history = []  # 存储历史记录
+        self.base_path = ""  # 存储当前路径
+
+        root.title(":)")
+        root.geometry("500x200")
+        root.resizable(False, False)
+
+        # 创建一个框架用于输入
+        input_frame = tk.Frame(root)
+        input_frame.grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
+
+        # 输入框1
+        label1 = tk.Label(input_frame, text="Enter Log Name:")
+        label1.grid(row=0, column=0)
+        self.entry1 = tk.Entry(input_frame)
+        self.entry1.grid(row=1, column=0)
+
+        # 输入框2
+        label2 = tk.Label(input_frame, text="Enter Axis-Number:")
+        label2.grid(row=2, column=0)
+        self.entry2 = tk.Entry(input_frame)
+        self.entry2.grid(row=3, column=0)
+
+        # 按钮
+        greet_button = tk.Button(input_frame, text="Go", width=15, height=1, command=self.on_button_click)
+        greet_button.grid(row=4, column=0, pady=(0, 5))
+
+        # 结果标签
+        self.result_label = tk.Label(input_frame, text="", fg="red")
+        self.result_label.grid(row=5, column=0)
+
+        # 设置拖放事件
+        root.drop_target_register(DND_FILES)
+        root.dnd_bind('<<Drop>>', self.on_file_drop)
+
+        # 历史记录框架
+        history_frame = tk.Frame(root)
+        history_frame.grid(row=0, column=1, padx=10, pady=10, sticky='nsew')
+
+        # 历史记录标签
+        self.history_label = tk.Label(history_frame, text="History:")
+        self.history_label.pack()
+
+        # 历史记录框
+        self.history_listbox = tk.Listbox(history_frame, width=30, height=6)
+        self.history_listbox.pack()
+        self.history_listbox.bind('<<ListboxSelect>>', self.on_history_select)
+
+        # 设置列权重
+        root.grid_columnconfigure(0, weight=1)  # 输入框所在列
+        root.grid_columnconfigure(1, weight=0)  # 历史记录列
+
+    def on_file_drop(self, event):
+        # 获取拖拽文件的路径
+        file_path = event.data
+
+        # 设置 base_path 为文件所在目录
+        self.base_path = os.path.dirname(file_path)
+        print(self.base_path)
+
+        # 获取文件名并更新输入框
+        file_name = os.path.basename(file_path)
+        self.entry1.delete(0, tk.END)
+        self.entry1.insert(0, file_name)
+
+    def on_button_click(self):
+        self.result_label.config(text="")
+
+        file_name = self.entry1.get()
+        file_path = os.path.join(self.base_path, file_name)
+ 
+        axis_number = self.entry2.get()
+
+        try:
+            axis_number = int(axis_number)
+        except ValueError:
+            self.result_label.config(text="Check your axis number plz:/")
+            return
+
+        # 判断 axis_number
+        if axis_number == 4 or axis_number == 3:
+            process_data(file_path)
+        else:
+            process_data2(file_path)
+
+        # 更新历史记录
+        self.update_history(file_name, axis_number)
+
+    def update_history(self, file_name, axis_number):
+        # 将当前输入添加到历史记录
+        entry = f"{file_name}, Axis: {axis_number}"
+        self.history.append(entry)
+
+        if len(self.history) > 6:
+            self.history.pop(0)
+
+        # 更新历史记录列表框
+        self.history_listbox.delete(0, tk.END)  # 清空列表框
+        for item in self.history:
+            self.history_listbox.insert(tk.END, item)
+
+    def on_history_select(self, event):
+        # 获取当前选择的历史记录
+        selected_index = self.history_listbox.curselection()
+        if selected_index:
+            selected_entry = self.history_listbox.get(selected_index)
+            file_name, axis_number = selected_entry.split(", Axis: ")
+            self.entry1.delete(0, tk.END)
+            self.entry1.insert(0, file_name)
+            self.entry2.delete(0, tk.END)
+            self.entry2.insert(0, axis_number)
+
+
+if __name__ == "__main__":
+    root = TkinterDnD.Tk()  # 创建TkinterDnD的Tk窗口
+    app = App(root)
+    root.mainloop()
